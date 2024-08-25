a81ff702cb64749ee8d8fdda974d500256692ac7
Mohammad Mahdi Moayeri <mmahdimoayeri@gmail.com>
2024-08-25 21:35:42 +0330

    update combinefetching file and add example for it

---
+++ Sources/SwiftNet/CombineFetching/CombineJSONFetchingExample.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by mohammadmahdi moayeri on 8/25/24.
+//
+
+import SwiftUI
+
+struct CombineFetchingExample: View {
+    @StateObject var vm = CombineFetching<DataModel>()
+    let url: String = "https://jsonplaceholder.typicode.com/posts"
+    
+    var body: some View {
+        List {
+            ForEach(vm.dataModels) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title)
+                        .font(.headline)
+                    Text(item.body)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+        .onAppear {
+            vm.fetchJSON(fromURL: url)
+        }
+    }
+}
+
+#Preview {
+    CombineFetchingExample()
+}
