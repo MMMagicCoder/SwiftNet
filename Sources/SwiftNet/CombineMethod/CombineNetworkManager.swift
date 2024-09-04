2d12518e7c4abf465ff214998a1b3985564289ea
Mohammad Mahdi Moayeri <mmahdimoayeri@gmail.com>
2024-09-04 20:07:51 +0330

    complete combinedownloadmanager class and add combinenetworkmanager class

--- Sources/SwiftNet/CombineMethod/CombineNetworkManager.swift
+++ Sources/SwiftNet/CombineMethod/CombineNetworkManager.swift
@@ -5,7 +5,7 @@ import Combine
 /**
  A utility class that handles fetching JSON data or raw data from a URL using Combine framework.
 
- The `CombineFetching` class is designed to fetch data from the internet using `URLSession` and Combine's `Publisher` mechanisms. It can decode JSON data into specified models conforming to the `FetchableModel` protocol or fetch raw data directly.
+ The `CombineFetching` class is designed to fetch data from the internet using `URLSession` and Combine's `Publisher` mechanisms. It can decode JSON data into specified models conforming to the `FetchableModel` protocol.
 
  - Note: The fetched data is handled asynchronously and returned via completion handlers.
 
@@ -15,7 +15,6 @@ import Combine
 
  - Usage:
    1. Call `fetchJSON(fromURL:completionHandler:)` to fetch and decode JSON data.
-   2. Call `fetchData(fromURL:completionHandler:)` to fetch raw data (e.g., images or other binary content).
  */
 
 public class CombineFetching<T: FetchableModel>: ObservableObject {
@@ -43,34 +42,6 @@ public class CombineFetching<T: FetchableModel>: ObservableObject {
                 }
             } receiveValue: { returnedData in
                 completionHandler(returnedData)
-            }
-            .store(in: &cancellables)
-    }
-    
-    /**
-     Fetches raw data from the specified URL and returns it through a completion handler.
-     
-     - Parameters:
-        - url: The URL string from which to fetch the raw data.
-        - completionHandler: A closure that is called upon completion of the fetch operation. The closure takes an optional `Data` object as its parameter, which is `nil` if the fetch fails.
-     */
-    public func fetchData(fromURL url: String, completionHandler: @escaping (Data?) -> ()) {
-        guard let url = URL(string: url) else { return }
-        
-        NetworkManager.dataTaskPublisher(fromURL: url)
-            .tryMap { data in
-                return data
-            }
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    print("Finished fetching data")
-                case .failure(let error):
-                    print("Failed to fetch data: \(error)")
-                    completionHandler(nil)
-                }
-            } receiveValue: { returnedData in
-               completionHandler(returnedData)
             }
             .store(in: &cancellables)
     }
