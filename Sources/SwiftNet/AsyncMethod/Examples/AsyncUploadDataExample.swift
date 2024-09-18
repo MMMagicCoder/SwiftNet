d454b43465ad2705eb981680fa07165287e76a18
Mohammad Mahdi Moayeri <mmahdimoayeri@gmail.com>
2024-09-18 08:38:17 +0330

    complete all async files

---
+++ Sources/SwiftNet/AsyncMethod/Examples/AsyncUploadDataExample.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by mohammadmahdi moayeri on 9/17/24.
+//
+
+import SwiftUI
+
+struct AsyncUploadingExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncUploadingExample()
+}
